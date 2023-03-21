ead56abf85d69ca107d16ce9e6ca627dc1e9473e
Nikita Kanaev <68945163+LuiPJ@users.noreply.github.com>
2023-03-21 10:49:48 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,8 @@
-var gulp = require('gulp');
-const gulpConnect = require('gulp-connect');
-const sass = require('gulp-sass')(require('sass'));
-
-
-gulp.task('default', function () {
-	gulp.src('./src/**/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./prod/style'))
-})
-
-gulp.task('sass:watch', function () {
-	gulp.watch('./src/**/*.scss', gulp.series('sass'))
-})
-
-function server() {
-	return gulpConnect.server({
-			host: '0.0.0.0',
-			port: 8080,
-			root: 'src/',
-			livereload: true
-	});
-}
-
-exports.server = server;
-
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass')(require('sass')); //Подключаем Sass пакет
+ 
+gulp.task('sass', function(){ // Создаем таск "sass"
+	return gulp.src('app/sass/main.sass') // Берем источник
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+});
